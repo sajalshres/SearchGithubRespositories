3f7fad738a095f2b1f5e4f4c3b3884b530c43834
sajal <sajal.shres@gmail.com>
2022-07-21 21:45:16 +0545

    Updated package

--- R/search_github_repositories.R
+++ R/search_github_repositories.R
@@ -13,31 +13,30 @@ search_github_repositories <-
     # urls
     github_api_url <- "https://api.github.com"
     search_endpoint <- "/search/repositories"
-
+    
     # build query params
     query_params <-
       paste(paste0("?q=", query),
-        paste0("sort=", sort),
-        paste0("order=", order),
-        sep = "&"
-      )
-
+            paste0("sort=", sort),
+            paste0("order=", order),
+            sep = "&")
+    
     print(paste("Url is", github_api_url, search_endpoint, query_params))
-
+    
     # make http request to Github Api
     response <-
       httr::GET(url = paste0(github_api_url, search_endpoint, query_params))
-
+    
     response_status_code <- httr::status_code(response)
-
+    
     # Stop if unexpected status code
     if (response_status_code != 200) {
       stop(paste0("Unexpected status", response_status_code, "encountered"))
     }
-
+    
     # Parse the JSON response
     response_parsed <- httr::content(response, as = "parsed")
-
+    
     # Print stats
     print(paste(
       "Search for",
@@ -46,6 +45,6 @@ search_github_repositories <-
       response_parsed$total_count,
       "results"
     ))
-
+    
     return(response_parsed)
-  }
+}
---
+++ tests/testthat/test-version.R
@@ -0,0 +1,3 @@
+test_that("Version works", {
+  expect_identical("Version: 0.0.1", versiob())
+})
